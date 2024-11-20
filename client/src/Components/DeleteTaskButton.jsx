8d100fec3b5e45195c0181b9ba28ae325de77f0c
JaviVC79 <135664449+JaviVC79@users.noreply.github.com>
2024-11-20 20:55:25 +0100

    Update DeleteTaskButton.jsx

--- client/src/Components/DeleteTaskButton.jsx
+++ client/src/Components/DeleteTaskButton.jsx
@@ -1,13 +1,14 @@
 import { deleteTask } from '../api/task.api.js';
+import { useNavigate } from 'react-router-dom'
 
 const DeleteTaskButton = ({ email, task }) => {
-
+    const navigate = useNavigate();
     return (
         <button
             className="bg-red-500 hover:bg-red-700 text-white font-bold rounded m-1 h-fit py-1 px-2"
             onClick={async () => {
                 await deleteTask(email, task.name, task.id);
-                window.location.reload();
+                navigate(0);
             }}
         >
             Delete
