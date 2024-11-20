9a022aeffbede4c4d4c8a9005cd46ebff8278e97
JaviVC79 <135664449+JaviVC79@users.noreply.github.com>
2024-11-20 20:44:35 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: process.env.PORT || 5173,
     host: '0.0.0.0'
   }
-})
+});
+
