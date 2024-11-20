6b2636f9d2a0cb72169db9657c8dd66af2d48a36
JaviVC79 <135664449+JaviVC79@users.noreply.github.com>
2024-11-20 17:52:43 +0100

    Update TaskPage.jsx

--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -1,9 +1,9 @@
-
+import homeImage from '../../public/home.jpg'
 
 function TaskPage() {
   return (
     <div className="flex inset-3 text-white justify-between py-16">
-        <img src="../public/home.jpg" alt="Home" className="rounded"/>
+        <img src={homeImage} alt="Home" className="rounded"/>
         <div>
           <h1 className="text-5xl py-4">TaskPage</h1>
           <p>Note your task everywhere</p>
