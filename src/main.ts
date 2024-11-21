2b4d497e789e9490eed7bf33d36cf996e5227bca
JaviVC79 <135664449+JaviVC79@users.noreply.github.com>
2024-11-21 09:20:19 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://first-project-nestjs-postgresql.onrender.com'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     preflightContinue: false,
