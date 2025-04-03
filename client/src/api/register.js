6ea930b0666560851c799623aaa87f1cfecb69bf
JaviVC79 <135664449+JaviVC79@users.noreply.github.com>
2025-04-03 02:58:09 +0200

    Update register.js

--- client/src/api/register.js
+++ client/src/api/register.js
@@ -5,7 +5,6 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const register = async (task) => {
   try {
     const response = await axios.post(`${BACKEND_URL}/users`, task);
-    print(response.status)
     if (response?.status == 201) {
       return 201;
     }
