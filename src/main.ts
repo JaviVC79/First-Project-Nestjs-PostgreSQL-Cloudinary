4341d20b98dabe4a9ab09d3799cc61a5423c0751
JaviVC79 <135664449+JaviVC79@users.noreply.github.com>
2025-05-04 02:40:54 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://tasks-eta-two.vercel.app/login'],
+    origin: ['https://tasks-eta-two.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     preflightContinue: false,
