ff23b942bbcb79a172bcba4c444b2ad893546af0
JaviVC79 <135664449+JaviVC79@users.noreply.github.com>
2025-05-04 15:44:34 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { StripeModule } from './stripe/stripe.module';
 
 @Module({
-  imports: [TaskModule, UsersModule, StripeModule, ConfigModule.forRoot()],
+  imports: [TaskModule, UsersModule, StripeModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
   providers: [],
 })
