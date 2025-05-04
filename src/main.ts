5959998d63c9a13490a3b15db5bd3a5957a9b8a4
JaviVC79 <135664449+JaviVC79@users.noreply.github.com>
2025-05-04 02:23:39 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [ORGINS],
+    origin: [ORIGINS],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     preflightContinue: false,
