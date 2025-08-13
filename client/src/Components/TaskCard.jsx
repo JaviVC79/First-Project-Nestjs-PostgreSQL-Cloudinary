29864b15fcd3be8481d5e407fbf1293082830ebb
JaviVC79 <135664449+JaviVC79@users.noreply.github.com>
2025-08-13 14:18:17 +0200

    Update TaskCard.jsx

--- client/src/Components/TaskCard.jsx
+++ client/src/Components/TaskCard.jsx
@@ -1,25 +1,27 @@
 import useTaskCardState from "../hooks/useTaskCardState.js";
 import NotEditableTasks from "./NotEditableTasks.jsx";
 import EditableTasks from "./EditableTasks.jsx";
-import { useState } from 'react'; 
+import { useState } from 'react'; 
 
 function TaskCard({ task }) {
-  const [isAddImageClicked, setIsAddImageClicked] = useState(false)
+  const [isAddImageClicked, setIsAddImageClicked] = useState(false);
   const {
     handleIsClick,
     isClicked,
   } = useTaskCardState(task);
 
   return (
-    <>
+    // Contenedor principal de la tarjeta, con estilos responsive
+    <div className="bg-gray-800 rounded-lg shadow-lg p-4 transition-transform transform hover:scale-105 min-w-[300px] max-w-sm w-full">
       {isClicked ?
         <EditableTasks task={task} handleIsClick={handleIsClick} /> :
         <NotEditableTasks task={task} handleIsClick={handleIsClick} isAddImageClicked={isAddImageClicked} setIsAddImageClicked={setIsAddImageClicked} />}
-    </>
+    </div>
   );
 }
 
 export default TaskCard;
+
 
 
 
